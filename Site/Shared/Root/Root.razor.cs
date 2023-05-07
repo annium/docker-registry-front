78543608be9428a8b85c3e17cc965854827f3e9a
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-05-07 11:34:26 +0300

    site: init appsettings.json

--- Site/Shared/Root/Root.razor.cs
+++ Site/Shared/Root/Root.razor.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
 
 namespace Site.Shared.Root;
 
 public partial class Root
 {
+    [Inject]
+    public IConfiguration Config { get; set; } = default!;
+
     protected override async Task OnInitializedAsync()
     {
-        Console.WriteLine("Root initialized");
+        Console.WriteLine($"Root initialized: {Config["registry"]}");
         await Task.CompletedTask;
     }
 }
